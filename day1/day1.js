e68fe1debc8d8561f22730d3d34c2f2966fb78ab
Davide Brognoli <davide.brognoli@gmail.com>
2021-12-01 21:50:14 +0100

    Day 1

---
+++ day1/day1.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+
+const puzzle1 = (data) => {
+  const checks = data.length - 1;
+  console.log(checks);
+  return data.filter((n, index) => {
+    return index < checks && n < data[index + 1];
+  }).length;
+}
+
+const puzzle2 = (data) => {
+  const checks = data.length - 2;
+  const groupedSums = data.reduce((acc, d, index) => {
+    if (index < checks) {
+      return [...acc, d + data[index + 1] + data[index + 2]]
+    }
+    return acc;
+  }, [])
+  return puzzle1(groupedSums);
+}
+
+const filePath = path.join(__dirname, 'input.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.log('An Error as occured: ', err)
+  } else {
+    const dataArray = data.toString().split("\n").map(i => parseInt(i, 10))
+    console.log(puzzle1(dataArray));
+    console.log(puzzle2(dataArray));
+  }
+})
